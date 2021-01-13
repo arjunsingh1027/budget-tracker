ebc9fa9aabb6b0f44bd889f527bdedbfa9181ce8
arjunsingh <arjunksingh1027@gmail.com>
2021-01-13 10:31:31 -0700

    big fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,8 +5,7 @@ const filesToCache = [
     "/",
     "/index.html",
     "/index.js",
-    "/db.js",
-    "/manifest.json",
+    "/manifest.webmanifest",
     "/styles.css",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
@@ -21,40 +20,52 @@ self.addEventListener("install", function (evt) {
             return cache.addAll(filesToCache);
         })
     );
+
+    self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        cache.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key !== STATIC_CACHE && key !== DATA_CACHE) {
+                        console.log("clearing cache", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
 });
 
 // fetch
 self.addEventListener("fetch", function (evt) {
-    if (evt.request.url.includes("/api/")) {
+    const { url } = evt.request;
+    if (url.includes("/api/")) {
         evt.respondWith(
             caches.open(DATA_CACHE).then(cache => {
-                return fetch(evt.request)
-                    .then(res => {
-                        if (res.status === 200) {
-                            cache.put(evt.request.url, res.clone());
-                        }
-                        return res;
-                    })
-                    .catch(err => {
-                        return cache.match(evt.request);
-                    });
+                return fetch(evt.request).then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request, response.clone());
+                    }
+                    return response;
+                }).catch(err => {
+                    return cache.match(evt.request);
+                });
             }).catch(err => console.log(err))
         );
-
-        return;
-    };
-
-    evt.respondWith(
-        fetch(evt.request).catch(function () {
-            return caches.match(evt.request).then(function (res) {
-                if (res) {
-                    return res;
-                } else if (evt.request.headers.get("accept").includes("text/html")) {
-                    return caches.match("/");
-                }
-            });
-        })
-    );
-
+    } else {
+        evt.respondWith(
+            caches.open(STATIC_CACHE).then(cache => {
+                return cache.match(evt.request).then(response => {
+                    return response || fetch(evt.request);
+                });
+            })
+        );
+    }
 });
 
